bccf00a71c1ac455c64b78964e6e9ef4fd0dc52b
Sai Krishna <saikrishnadrs@gmail.com>
2018-12-07 11:47:25 -0500

    commit

--- EventReg/SignedOut.Designer.cs
+++ EventReg/SignedOut.Designer.cs
@@ -36,17 +36,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 37);
+            this.label1.Location = new System.Drawing.Point(29, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(250, 13);
+            this.label1.Size = new System.Drawing.Size(349, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "The User Has been Signed Out. To Login Click Her";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(43, 87);
+            this.button1.Location = new System.Drawing.Point(57, 114);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(89, 32);
+            this.button1.Size = new System.Drawing.Size(119, 42);
             this.button1.TabIndex = 1;
             this.button1.Text = "Admin";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,9 +56,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(160, 87);
+            this.button2.Location = new System.Drawing.Point(213, 114);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(89, 32);
+            this.button2.Size = new System.Drawing.Size(119, 42);
             this.button2.TabIndex = 2;
             this.button2.Text = "User";
             this.button2.UseVisualStyleBackColor = true;
@@ -64,12 +67,15 @@
             // 
             // SignedOut
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(379, 341);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI Symbol", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "SignedOut";
             this.Text = "SignedOut";
             this.ResumeLayout(false);
